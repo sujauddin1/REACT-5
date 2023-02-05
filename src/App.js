b8d81c1ec296a8d0fac78923fd3a93fb1ad2f215
sujauddin <sujauddinkazi282538@gmail.com>
2023-02-05 08:33:15 +0530

    REACT 5TH APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>THIS IS MY 5TH REACT APP</h1>
+        <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt=""/>
+        
     </div>
   );
 }
